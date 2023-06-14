9bae818a5ca24a107bb9c7823300735eec186158
louis <louis@ATHENA>
2023-06-14 17:56:01 +0200

    Add project files.

---
+++ SuggestionAppLibrary/Models/StatusModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuggestionAppLibrary.Models
+{
+    public class StatusModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string StatusName { get; set; }
+        public string StatusDescription { get; set; }
+    }
+}
